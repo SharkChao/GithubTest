a2fb6de009ccb22f4292f66bb6505728bce40fb0
sharkchao <827623353@qq.com>
2019-04-19 22:25:56 +0800

    master_1

--- app/src/main/java/com/hupu/first/githubtest/MainActivity.java
+++ app/src/main/java/com/hupu/first/githubtest/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("a_debug","");
+
+        System.currentTimeMillis();
     }
 }
